dc5d081c7f5dca27a704258db8bfd9f6b48d056d
Leon <lmshimizu@gmail.com>
2021-10-24 23:02:03 -0700

    added seed dump

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Part.create!([
+  {name: "12 pc Instinct® Soft Grip Combination Screwdriver Set (Green)", price: "323.0", og_website: "https://shop.snapon.com/", ships: false},
+  {name: "Quick-Flow Pressure Brake Bleeder Kit", price: "595.0", og_website: "https://shop.snapon.com/", ships: false},
+  {name: "18 V 2,700 Lumen Cordless Floodlight (Tool Only), Red", price: "219.95", og_website: "https://shop.snapon.com/", ships: false},
+  {name: "1/4 Drive High-Precision Torque Wrench (40-200 in-lb)", price: "0.0", og_website: "https://shop.snapon.com/", ships: false},
+  {name: "18 V 1/2 Drive Brushless Cordless Impact Wrench Kit (Red)", price: "971.95", og_website: "https://shop.snapon.com/", ships: false}
+])
